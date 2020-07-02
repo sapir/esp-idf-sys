157d89a184f00725d14d451dc7128275cd1641bd
Markus Reiter <me@reitermark.us>
2020-07-02 08:23:33 +0200

    Use `libc` crate instead of redefining types.

--- src/lib.rs
+++ src/lib.rs
@@ -3,21 +3,6 @@
 #![allow(non_camel_case_types)]
 #![allow(non_snake_case)]
 
-pub mod libc {
-  pub enum c_void {}
-  pub type c_uchar = u8;
-  pub type c_schar = i8;
-  pub type c_char = i8;
-  pub type c_short = i16;
-  pub type c_ushort = u16;
-  pub type c_int = i32;
-  pub type c_uint = u32;
-  pub type c_long = i32;
-  pub type c_ulong = u32;
-  pub type c_longlong = i64;
-  pub type c_ulonglong = u64;
-}
-
 include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
 
 #[cfg(target_device = "esp8266")]
